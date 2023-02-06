c483a3e77ad290601427c812461cae1c12dced16
altaypower <altaypower@gmail.com>
2023-02-06 22:28:50 +0700

    update main_double.py

--- main_double.py
+++ main_double.py
@@ -2,9 +2,6 @@ import os
 import dotenv
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from gb_parse.spiders.autoyoula import AutoyoulaSpider
-#from gb_parse.spiders.instagram7 import InstagramSpider
-#from gb_parse.spiders.zillow import ZillowSpider
 from gb_parse.spiders.instagram_double import InstagramSpider
 
 if __name__ == "__main__":
